2046742d5b44a8b4ee7a875ca3ee24cbf67c6d71
Christof Schramm <kryo@vm-10-155-208-4.cloud.mwn.de>
2016-06-11 10:55:56 +0200

    Helper module for dealing with text ranges

---
+++ src/Thesis/Data/Text/PositionRange.hs
@@ -0,0 +1,43 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Thesis.Data.Text.PositionRange ( PositionRange (..)
+                                      , Position (..)
+
+                                      , textFrom
+                                      , textUntil
+                                      , textInRange
+                                      ) where
+
+import Data.Conduit.Attoparsec
+import Data.Text (Text)
+import qualified Data.Text as Text
+
+-- | Text in range. (Includes both start and stop position)
+textInRange :: PositionRange -> Text -> Text
+textInRange (PositionRange start stop) t =
+  textFrom start $ textUntil stop t
+
+-- | Text from a certain position
+textFrom :: Position -> Text -> Text
+textFrom (Position l c) t =
+  case textLines of
+    []   -> ""
+    l:ls -> let l' = Text.drop (c-1) l
+            in Text.unlines $ if l' == ""
+                              then ls
+                              else l':ls
+  
+  where
+    textLines = drop (l-1) $ Text.lines t
+
+-- | Text up to a certain position (inclusive)
+textUntil :: Position -> Text -> Text
+textUntil (Position l c) t =
+  case reverse textLines of
+    []   -> ""
+    l:ls -> let l' = Text.take (c) l
+            in Text.unlines $ if l' == ""
+                              then ls
+                              else reverse $ l':ls
+  
+  where
+    textLines = take l $ Text.lines t
