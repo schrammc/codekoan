6fcb9289e0fdeaf983398e50385a01f92049b178
Christof Schramm <kryo@vm-10-155-208-4.cloud.mwn.de>
2016-06-13 20:29:24 +0200

    TEST: added a test for compressed trie

---
+++ test/Thesis/CompressedTrieSpec.hs
@@ -0,0 +1,16 @@
+module Thesis.CompressedTrieSpec where
+
+import Test.Hspec
+
+import Thesis.CompressedTrie
+
+import Data.Vector as V
+
+spec = do
+  describe "Thesis.CompressedTrie" $ do
+    aFewWords
+      
+aFewWords = it "should correctly read a few words" $ do
+  let tr = buildTrie [("abcdef",1), ("abc",2),("abcdxxy",3)]
+  print tr
+  (V.length $ wordsInTrie tr) `shouldBe` 3
