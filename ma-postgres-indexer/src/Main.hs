f7644b1aa70a631411fd2e4f269229ea5bf27a17
Christof Schramm <kryo@vm-10-155-208-4.cloud.mwn.de>
2016-08-10 15:46:17 +0200

    FEATURE: Executable ma-postgres-indexer for dump to postgres export

---
+++ ma-postgres-indexer/src/Main.hs
@@ -0,0 +1,41 @@
+-- | A very simple executable that dumps all the data from the Posts file of a
+-- stackoverflow xml dump into a postgresql database
+--
+--  Author: Christof Schramm
+module Main where
+
+import Thesis.Data.Stackoverflow.Dump
+import Database.PostgreSQL.Simple
+
+import System.Environment
+import System.Directory
+
+main = do
+  args <- getArgs
+  case args of
+    [file, host, port, user, password, database] -> do
+      let connectInfo = ConnectInfo{ connectHost = host
+                                   , connectPort = read port
+                                   , connectUser = user
+                                   , connectPassword = password
+                                   , connectDatabase = database
+                                   }
+
+      -- Check for existence of the XML dump file
+      fileExists <- doesFileExist file
+      if fileExists
+        then do
+          putStrLn "Read parameters, found dump file."
+          putStrLn "Beginning transfer..."
+          dumpToPostgres file connectInfo
+          putStrLn "Done!"
+        else putStrLn "Couldn't find dump file!"
+    _ -> putStrLn msg
+  
+  where
+    msg = unlines [ "Call with <dump file path> <postgre host> <postgre port> <postgre user> <postgre password> <postgre database>"
+                  , ""
+                  , "Dumps all posts from a stackoverflow XML dump into a PostgreSQL database."
+                  ]
+
+      
--- ma-xml-analyzer/app/Main.hs
+++ ma-xml-analyzer/app/Main.hs
@@ -10,30 +10,11 @@ import Data.Char (chr)
 import qualified Data.Vector as V
 
 import Thesis.Search.CompressedTrie
+import Thesis.Data.Stackoverflow.Dump
 
 import System.Random
 import Control.Monad
 
 main :: IO ()
 main = do
-  tree <- force <$> randomSuffixTree 1000
-
-  print $ length (wordsInTrie' tree)
-  print "Waiting"
-
-  threadDelay $ 1000 * 1000
-
-  print $ length (wordsInTrie' tree)
-
-randomSuffixTree :: Int -> IO (CompressedTrie Char Int)
-randomSuffixTree n = do
-  strs <- replicateM n randomString
-  let suffixTrees = (\(s,k) -> buildSuffixTrie Nothing s k) <$> zip (V.fromList <$> strs) [0..]
-  return $ foldl1 mergeTries suffixTrees
-
-
-
-randomString :: IO String
-randomString = do
-  n <- randomRIO (20, 500)
-  replicateM n $ randomRIO (chr 0, chr 100)
+  return ()
