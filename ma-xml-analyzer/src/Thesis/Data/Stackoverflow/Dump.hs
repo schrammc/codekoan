c343cf4de8769e008d9569d02bd055133fab8eb0
Christof Schramm <kryo@vm-10-155-208-4.cloud.mwn.de>
2016-08-10 15:45:44 +0200

    FEATURE: Dump to postgres export

--- ma-xml-analyzer/src/Thesis/Data/Stackoverflow/Dump.hs
+++ ma-xml-analyzer/src/Thesis/Data/Stackoverflow/Dump.hs
@@ -1,11 +1,16 @@
+-- | This module provides a conduit source of 'StackoverflowPost's from
+-- a Stackoverflow XML dump
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
 module Thesis.Data.Stackoverflow.Dump where
 
 import Thesis.Data.Stackoverflow.StackoverflowPost
+
 import Data.Conduit
 import Control.Monad.Trans.Resource
 
-import           Text.XML.Stream.Parse
+import Text.XML.Stream.Parse
 
--- | Create a source of 'StackoverflowPost' s from a file
+-- | Create a source of 'StackoverflowPost's from an XML dump file
 postSource :: FilePath -> Source (ResourceT IO) StackoverflowPost
 postSource xmlFilePath = parseFile def xmlFilePath =$= parsePosts
---
+++ ma-xml-analyzer/src/Thesis/Data/Stackoverflow/Dump/Export/Postgres.hs
@@ -0,0 +1,122 @@
+-- | This module provides functionality to store posts from an XML data dump of
+-- Stackoverflow posts in a PostgreSQL database.
+--
+-- Author: Christof Schramm
+--
+
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
+
+module Thesis.Data.Stackoverflow.Dump.Export.Postgres (dumpToPostgres) where
+
+import Thesis.Data.Stackoverflow.StackoverflowPost
+import Thesis.Data.Stackoverflow.Answer
+import Thesis.Data.Stackoverflow.Question
+import Thesis.Data.Stackoverflow.Dump
+
+import Data.Conduit hiding (connect)
+import qualified Data.Conduit.List as CL
+import Control.Monad.Trans.Resource
+import Control.Monad.IO.Class (liftIO)
+
+import Database.PostgreSQL.Simple
+
+import Control.Monad (when)
+import Control.Concurrent.MVar
+
+-- | Writes the information in the Stackoverflow XML dump to a specified
+-- postgresql database. Uses
+dumpToPostgres :: FilePath -> ConnectInfo -> IO ()
+dumpToPostgres dumpPath connInfo = do
+  connection <- connect connInfo
+  
+  makePostgresSchema connection
+
+  let chunkSize = 500
+
+  counter <- newMVar (0 :: Int)
+
+  runResourceT $ do
+    postSource dumpPath $$
+      chunks chunkSize
+      =$= (CL.iterM $ \posts -> liftIO $ do
+              let n = length posts
+              c <- takeMVar counter
+              putMVar counter (c+n)
+
+              when (c `mod` 25000 == 0) $ print c
+              
+              writePosts connection posts) 
+      =$= awaitForever return
+
+  return ()
+
+-- | Split the conduit into chunks of length n.
+--
+-- If the conduit has less than n values left then the last chunk will have a
+-- size less than n.
+chunks :: Monad m =>
+          Int -- ^ Chunk size values < 1 will be set to 1
+          -> ConduitM a [a] m ()
+chunks n = do
+  go [] (max 1 n) >>= yield
+  chunks n
+  where
+    go xs k | k == 0 = return xs
+            | otherwise = do
+              nextMaybe <- await
+              case nextMaybe of
+                Just next -> go (next:xs) (k-1)
+                Nothing   -> return xs
+      
+-- | Create a schema for the dump data in Postgresql
+makePostgresSchema :: Connection -> IO ()
+makePostgresSchema conn = do
+  execute_ conn "DROP TABLE IF EXISTS answers"
+  execute_ conn "DROP TABLE IF EXISTS questions"
+
+  execute_ conn "CREATE TABLE answers(id INTEGER NOT NULL, body TEXT, rating INTEGER, parent INTEGER NOT NULL, PRIMARY KEY(id))"
+  execute_ conn "CREATE TABLE questions(id INTEGER NOT NULL, tags TEXT[], body TEXT, title TEXT, rating INTEGER, PRIMARY KEY(id))"
+  return ()
+
+-- | Write a set of posts to the given database
+writePosts :: Connection -> [StackoverflowPost] -> IO ()
+writePosts conn posts = do
+  writeAnswers conn answers
+  writeQuestions conn questions
+  return ()
+  where
+    (answers, questions) = foldl (\(as,qs) -> \post -> case post of
+                                     PostAnswer a   -> (a:as, qs  )
+                                     PostQuestion q -> (as  , q:qs)
+                                     _ -> (as,qs)
+                                 ) ([],[]) posts
+
+-- | Write a set of answers to the "answers" table of the given database
+writeAnswers :: Connection -> [Answer] -> IO ()
+writeAnswers connection as = do
+  let transformed = do
+        Answer{..} <- as
+        return $ ( answerIdInt answerId
+                 , answerBody
+                 , answerRating
+                 , answerParent
+                 )
+  executeMany connection "INSERT INTO answers VALUES (?,?,?,?)" transformed
+ 
+  return ()
+
+-- | Write a set of questions to the "questions" table of the given database
+writeQuestions :: Connection -> [Question] -> IO ()
+writeQuestions connection qs = do
+  let transformed = do
+        Question{..} <- qs
+        return ( questionIdInt questionId
+               , questionTags
+               , questionBody
+               , questionTitle
+               , questionRating
+               )
+  executeMany connection "INSERT INTO questions VALUES (?,?,?,?,?)" transformed
+
+  return ()
