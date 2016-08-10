6811ce221944ce52b8cbaf475867ce64f1fcd64f
Christof Schramm <kryo@vm-10-155-208-4.cloud.mwn.de>
2016-08-10 20:31:41 +0200

    Change dictionary to interface-like

--- ma-xml-analyzer/src/Thesis/Data/Stackoverflow/Dictionary.hs
+++ ma-xml-analyzer/src/Thesis/Data/Stackoverflow/Dictionary.hs
@@ -1,59 +1,38 @@
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE GADTs #-}
 module Thesis.Data.Stackoverflow.Dictionary where
 
-import Data.Text hiding (concat)
-
-import Data.Binary
-import GHC.Generics (Generic)
-
-import qualified Data.ByteString as BS
-import qualified Data.ByteString.Lazy as BL
-
+import Data.Text
 import qualified Data.Set as S
-
-import           Data.IntMap.Strict (IntMap)
-import qualified Data.IntMap.Strict as IM
-
 import Thesis.Data.Stackoverflow.Answer
 import Thesis.Data.Stackoverflow.Question
-
-import Control.DeepSeq
+import Control.Monad.Trans.Maybe
+import Control.Monad.Catch
 
 -- | A helper data structure that allows to find tags for all questions in a
 -- stackoverflow data dump somewhat efficiently.
 --
--- A binary instance is provided, so that the data can be persisted in binary
--- form to a file, as recomputing it for repeated applications is prohibitively
--- expensive.
-data DataDictionary = DataDictionary { dictAnswerParents :: !(IntMap Int)
-                                     , dictQuestionTags :: !(IntMap (S.Set Int))
-                                     , dictTagTable :: !(IntMap Text)
-                                     }
-                      deriving (Generic, NFData, Binary)
-         
--- | Get the text tags for an answer's parent question
-answerRootTags :: DataDictionary -> AnswerId -> Maybe (S.Set Text)
-answerRootTags (DataDictionary{..}) (AnswerId aid) = do
-  parent <- IM.lookup aid dictAnswerParents
-  tags <- IM.lookup parent dictQuestionTags
-  tagStrings <- mapM (\tag -> IM.lookup tag dictTagTable) (S.toList tags)
-  return $ S.fromList tagStrings
+-- Polymorphic over a monad that is used to access the given data
+data DataDictionary m where
+  DataDictionary :: (MonadThrow m) =>
+                    { answerParent :: AnswerId -> MaybeT m QuestionId
+                    , questionTags :: QuestionId -> MaybeT m (S.Set Text)
+                    } -> DataDictionary m
 
--- | Get the text tags for an answer's parent question
-answerParent :: DataDictionary -> AnswerId -> Maybe (QuestionId)
-answerParent (DataDictionary{..}) (AnswerId aid) = do
-  parent <- IM.lookup aid dictAnswerParents
-  return $ QuestionId parent
+-- | Get the tags of the parent question to the given answer
+--
+-- Nothing if the given 'AnswerId' is not in the dictionary
+answerRootTags :: DataDictionary m -> AnswerId -> MaybeT m (S.Set Text)
+answerRootTags DataDictionary{..} aId = do
+  parent <- answerParent aId
+  questionTags parent
 
 -- | See if the answer's parent question is tagged with the given tag
-answerWithTag :: DataDictionary -> Text -> AnswerId -> Bool
-answerWithTag dict tag i = maybe False (S.member tag) (answerRootTags dict i)
-
-readDictionary :: FilePath -> IO DataDictionary
-readDictionary dictPath = do
-  content <- BS.readFile dictPath
-  return $!! decode (BL.fromChunks [content])
-
-emptyDictionary = DataDictionary IM.empty IM.empty IM.empty
+answerWithTag :: DataDictionary m -> Text -> AnswerId -> m Bool
+answerWithTag dict@DataDictionary{..} tag aId = do
+  maybeTags <- runMaybeT $ answerRootTags dict aId
+  case maybeTags of
+    Nothing -> return False
+    Just tags -> return $ S.member tag tags
