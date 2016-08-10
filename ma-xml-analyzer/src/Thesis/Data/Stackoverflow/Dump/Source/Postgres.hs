e4f2b8443ecd24740df29f4b6eeb0fd535a0e6f2
Christof Schramm <kryo@vm-10-155-208-4.cloud.mwn.de>
2016-08-10 21:05:32 +0200

    FEATURE: Add postgres random access to answers and questions

--- ma-xml-analyzer/src/Thesis/Data/Stackoverflow/Dump/Source/Postgres.hs
+++ ma-xml-analyzer/src/Thesis/Data/Stackoverflow/Dump/Source/Postgres.hs
@@ -1,13 +1,16 @@
+{-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE OverloadedStrings #-}
 module Thesis.Data.Stackoverflow.Dump.Source.Postgres where
 
 import Thesis.Data.Stackoverflow.Answer
-
+import Thesis.Data.Stackoverflow.Question
 import Database.PostgreSQL.Simple
 
 import Data.Conduit hiding (connect)
 import Control.Monad.Trans.Resource
+import Control.Monad.Trans.Maybe
 import Control.Monad.IO.Class
+import Control.Monad.Trans.Class
 import Control.Monad
 
 -- | Create a source of 'StackoverflowPost's from an XML dump file
@@ -26,3 +29,38 @@ answerSource connection = go 0
           maxId = maximum $ (\(i, _, _, _) -> i)<$> results
       forM_ answers yield
       go maxId
+
+-- | Get an answer from postgres by id
+getAnswer :: Connection -> AnswerId  -> MaybeT IO Answer
+getAnswer connection AnswerId{..} = do
+  results <- lift $ query connection
+                          "SELECT * FROM answers WHERE answers.id = ?"
+                          [answerIdInt]
+  let answers = do
+        (aId, aBody, aRating, aParent) <- results
+        return $ Answer (AnswerId aId)
+                        aBody
+                        aRating
+                        aParent
+  case answers of
+    [] -> MaybeT $ return Nothing
+    (a:_) -> return a
+  
+-- | Get a question from postgres by id
+getQuestion :: Connection -> QuestionId  -> MaybeT IO Question
+getQuestion connection QuestionId{..} = do
+  results <- lift $ query connection
+                          "SELECT * FROM questions WHERE questions.id = ?"
+                          [questionIdInt]
+  let questions = do
+        (qId, qTags, qBody, qTitle, qRating) <- results
+        return $ Question (QuestionId qId)
+                          qBody
+                          qTitle
+                          qRating
+                          qTags
+  case questions of
+    [] -> MaybeT $ return Nothing
+    (q:_) -> return q
+  
+  
