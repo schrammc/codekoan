5ec66269553a93cafbaa06f8e624d4c50afaba0f
Christof Schramm <kryo@vm-10-155-208-4.cloud.mwn.de>
2016-09-01 18:01:27 +0200

    FEATURE: Python parser

--- ma-xml-analyzer/src/Thesis/CodeAnalysis/Language/Python.hs
+++ ma-xml-analyzer/src/Thesis/CodeAnalysis/Language/Python.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE MultiWayIf #-}
 -- |
 -- Copyright: Christof Schramm 2016
 -- License: All rights reserved
@@ -18,12 +19,17 @@
 module Thesis.CodeAnalysis.Language.Python where
 
 import           Control.Applicative
--- import           Control.Monad
+import           Control.Monad
+import           Control.Monad.Trans.Class
+import           Control.Monad.Trans.State
 import           Data.Char
 import           Data.Hashable (Hashable)
 import           GHC.Generics (Generic)
 import           Thesis.CodeAnalysis.Language
 import           Thesis.CodeAnalysis.Language.CommonTokenParsers
+import           Data.Maybe(fromJust)
+
+import Debug.Trace
 
 import qualified Data.Text as Text
 
@@ -34,7 +40,8 @@ data Python
 python :: Language PyToken Python
 python = Language{ languageFileExtension = ".py"
                  , tokenize = tokenizePy
-                 , normalize = id
+                 , normalize = \LanguageText{..} ->
+                     LanguageText $ Text.replace "\t" "    " langText
                  , isTokenIdentifier = (== PyTokenIdentifier)
                  , removeComments = LanguageText
                  }
@@ -46,6 +53,7 @@ data PyToken = PyTokenIndent
              | PyTokenEQ
              | PyTokenAssign
              | PyTokenNot
+             | PyTokenIn
              | PyTokenBinAnd
              | PyTokenAnd
              | PyTokenBinOr
@@ -67,51 +75,108 @@ data PyToken = PyTokenIndent
              | PyTokenQuestion
              | PyTokenSemicolon
              | PyTokenKeyword
+             | PyTokenClass
+             | PyTokenImport
              | PyTokenBreak
              | PyTokenLoopWord
              | PyTokenIdentifier
-             | PyTokenPrimitive
              | PyTokenNumber
              | PyTokenStringLiteral
              | PyTokenCharacterLiteral
              | PyTokenModifier
              deriving (Eq, Ord, Show, Generic, Hashable)
 
-data SpaceType = Tabs
-               | Spaces
+-- | State that the will use internally.
+data ParserState = ParserState{ blockWidth :: Maybe Int
+                              , indentationLevel :: Int
+                              }
 
 data IndentationFault = IndentationFault
 
+-- | Tokenize a piece of python code. It is critical, that this code is
+-- normalized beforehand. Normalization eliminates all tab characters in the
+-- code and replaces them with tabs. The normalized code is then /much/ easier
+-- to parse. Not that this parser will accept correct python 3 code but also a
+-- lot of code that violates some of the indentation rules or things that python
+-- might enforce.
 tokenizePy :: LanguageText Python -> Maybe (TokenVector PyToken Python)
-tokenizePy LanguageText{..} = undefined
+tokenizePy LanguageText{..} = buildTokenVector <$> parsedResult
   where
-      parsePy indentWidth spaceType = do
-        spaces <- validateSpaces spaceType
-        case spaces of
-          Left IndentationFault -> return Nothing
-          Right spaceTokens  -> do
-            tks <- many' lenParser
-            numberOfBreaks <- eols
-            undefined
-      lenParser :: Parser (Int, Maybe PyToken)
-      lenParser = do
-        (txt, token) <- AP.match tokenOrComment
-        return $ (Text.length txt, token)
+    parsedResult = case AP.parseOnly parseCode langText of
+      Right x -> Just x
+      Left _  -> Nothing
+    
+    parseCode :: Parser [(Int, Maybe PyToken)]
+    parseCode = evalStateT parsePy (ParserState Nothing 0) 
 
-      tokenOrComment = ((const Nothing) <$> commentOrSpace) <|> (Just <$> tokenP)
-      commentOrSpace = pythonLineComment <|>
-                       (AP.takeWhile isHorizontalSpace *> pure ())
-      eols = do
-        (txt, _) <- AP.match (AP.takeWhile isEndOfLine)
-        return $ Text.length txt
+    -- | Recursive parser that parses an entire file while strining along some
+    -- parser state. That state contains information on the depth of the current
+    -- code block. This parser assumes that it always starts at a fresh line.
+    parsePy :: StateT ParserState Parser [(Int, Maybe PyToken)]
+    parsePy = do
+      -- Generate indentation tokens if necessary
+      spaces <- getSpaces
 
-      validateSpaces :: SpaceType
-                     -> Parser (Either IndentationFault [(Int, Maybe PyToken)])
-      validateSpaces spaceType =  do
-        txt <- AP.takeWhile isHorizontalSpace
-        case spaceType of
-          Spaces -> undefined
-          Tabs   -> undefined
+      -- Parse the line's contents
+      tks <- lift $ AP.many1 lenParser
+
+      --  Parse a number of linebreaks after the content of the line
+      lbs <- (lift $ ((:[]) <$> eols)) <|> (return [])
+
+      
+      rest <- parsePy <|> (lift $ endOfInput *> pure [])
+
+      let tokens = case tks of
+            [] -> (filter ((> 0) . fst) spaces) ++ lbs
+            _  -> spaces ++ tks ++ lbs
+      return $ tokens ++ rest
+
+    -- | Parses tokens, comments and horizontal whitespace along with
+    -- information on the length of the consumed string.
+    lenParser :: Parser (Int, Maybe PyToken)
+    lenParser = do
+      (txt, token) <- AP.match tokenOrComment
+      return $ (Text.length txt, token)
+
+    tokenOrComment = ((const Nothing) <$> commentOrSpace) <|> (Just <$> tokenP)
+    commentOrSpace = pythonLineComment <|>
+                     (AP.takeWhile1 isHorizontalSpace *> pure ())
+    eols = do
+      (txt, _) <- AP.match (AP.takeWhile1 isEndOfLine)
+      return $ (Text.length txt, Nothing)
+
+    -- | This helper function is for parsing spaces at the beginning of
+    -- lines. It generates the 0-width indentatoin tokens.
+    getSpaces:: StateT ParserState Parser [(Int, Maybe PyToken)]
+    getSpaces = do
+      spaces <- lift $ AP.takeWhile isHorizontalSpace
+
+      st@ParserState{..} <- get
+      let n = Text.length spaces
+          same = n == indentationLevel
+          close = n < indentationLevel
+          open  = n > indentationLevel
+
+      when (blockWidth == Nothing && n > 0) $ put st{blockWidth = Just n}
+
+      st@ParserState{..} <- get
+      let valid = case blockWidth of
+                    Nothing -> True
+                    Just x  -> traceShow (gcd n x) gcd n x > 1
+          delta = indentationLevel - n
+
+      put st{indentationLevel = n}
+
+      if valid
+      then if | n == 0 && same -> return []
+              | same -> return [(n, Nothing)]
+              | open -> return [(0, Just PyTokenIndent), (n, Nothing)]
+              | close ->
+                  let lst = replicate (delta `div` (fromJust blockWidth))
+                                      (0,Just PyTokenUnindent)
+                  in return $ lst ++ [(n,Nothing)]
+              | n > 0 && open -> undefined
+      else fail "Indentation fault"
 
 tokenP :: Parser PyToken
 tokenP = "<"      *> pure PyTokenLT
@@ -120,10 +185,19 @@ tokenP = "<"      *> pure PyTokenLT
          <|> "="  *> pure PyTokenAssign
          <|> ":"  *> pure PyTokenColon
          <|> ","  *> pure PyTokenComma
+         <|> "."  *> pure PyTokenDot
+         <|> "+"  *> pure PyTokenAdd
+         <|> "-"  *> pure PyTokenSub
          <|> "("  *> pure PyTokenLParen
          <|> ")"  *> pure PyTokenRParen
          <|> "["  *> pure PyTokenLBrack
          <|> "]"  *> pure PyTokenRBrack
+         <|> "not" *> pure PyTokenNot
+         <|> "and" *> pure PyTokenAnd
+         <|> "in"  *> pure PyTokenIn
+         <|> "or"  *> pure PyTokenOr
+         <|> "class" *> pure PyTokenClass
+         <|> "import" *> pure PyTokenImport
          <|> pyStringLiteral
          <|> pyCharacterLiteral
          <|> identifier
