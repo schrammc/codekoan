8cee5aeb32acd419dc452fc29396a5ae7926f636
christof schramm <christof.schramm@gmx.de>
2016-09-02 18:17:04 +0200

    REFACTOR: Use common block analysis module for java

--- ma-xml-analyzer/src/Thesis/CodeAnalysis/Language/Java/Internal/BlockAnalysis.hs
+++ ma-xml-analyzer/src/Thesis/CodeAnalysis/Language/Java/Internal/BlockAnalysis.hs
@@ -11,50 +11,13 @@
 {-# LANGUAGE RecordWildCards #-}
 module Thesis.CodeAnalysis.Language.Java.Internal.BlockAnalysis where
 
-import           Data.Foldable (foldl')
 import qualified Data.Vector as V
+import           Thesis.CodeAnalysis.Semantic.Blocks (BlockData)
 import           Thesis.CodeAnalysis.Language.Java.Internal.Tokens
-import           Thesis.CodeAnalysis.Semantic.Blocks
-import           Thesis.Data.Range
+import           Thesis.CodeAnalysis.Language.Internal.StandardTokenBlockAnalysis
 
-javaBlockData :: V.Vector Token -> V.Vector Token -> BlockData Token
-javaBlockData queryTokens fragmentTokens =
-  BlockData { queryRelation    = javaRelation queryTokens
-            , fragmentRelation = javaRelation fragmentTokens
-            , queryBlockString    = javaBlockStringInRegion queryTokens
-            , fragmentBlockString = javaBlockStringInRegion fragmentTokens
-            }
-
-javaBlockStringInRegion :: V.Vector Token -> Range t -> [BlockDelim]
-javaBlockStringInRegion tks (Range{..}) = javaBlockString subSlice
-  where
-    subSlice = V.unsafeSlice a (b - a) tks
-    a = normalizeV tks rangeStart
-    b = normalizeV tks rangeEnd
-
-javaBlockString :: V.Vector Token -> [BlockDelim]
-javaBlockString tks = do
-  token <- V.toList tks
-  case token of
-    TokenLBrace -> return BlockStart
-    TokenRBrace -> return BlockEnd
-    _ -> []
-
-javaRelation :: V.Vector Token -> Int -> Int -> BlockRelation
-javaRelation tks a b | V.length tks == 0 = inSameBlock
-                     | a == b = inSameBlock
-                     | otherwise = foldl' f inSameBlock relevantRegion
-  where
-    relevantRegion = V.unsafeSlice x (y-x) tks
-    f :: BlockRelation -> Token -> BlockRelation
-    f br@BlockRelation{..} TokenLBrace | down > 0 = br{down=down-1}
-                                       | otherwise = br{up=up+1}
-    f br@BlockRelation{..} TokenRBrace | up > 0 = br{up = up-1}
-                                       | otherwise = br{down = down+1}
-    f br _ = br
-
-    -- x and y are the minimum of a and b and the maximum of a and b after a
-    -- normalization that puts each value in the interval of 
-    x = normalizeV tks $ min a b
-    y = normalizeV tks $ max a b
-
+-- | A java implementation of 'standardBlockData'
+javaBlockData :: V.Vector Token
+              -> V.Vector Token
+              -> BlockData Token
+javaBlockData =  standardBlockData TokenLBrace TokenRBrace
