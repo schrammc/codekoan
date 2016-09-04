dd8be172d1ed7cde43176e3e896f93dd5469e99b
christof schramm <christof.schramm@gmx.de>
2016-09-04 14:52:45 +0200

    FEATURE: Query type

---
+++ ma-messaging/src/Thesis/Messaging/Query.hs
@@ -0,0 +1,35 @@
+-- |
+-- Author: Christof Schramm 2016
+-- License: All rights reserved
+--
+-- A type for a query document with a handy builder function
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DeriveAnyClass #-}
+module Thesis.Messaging.Query
+       ( -- * A query type
+         Query(..)
+         -- * Building a query
+       , buildQuery
+       )where
+
+import Data.Text (Text)
+import GHC.Generics (Generic)
+
+import Data.Aeson
+
+import Thesis.Search.Settings
+
+-- | A single query document
+data Query = Query { queryText :: Text
+                     -- ^ Text representation of the query
+                   , queryLanguage :: Text
+                     -- ^ Programming language of the query
+                   , queryId :: Maybe Integer
+                   , querySettings :: SearchSettings
+                     -- ^ Settings for the querie's search
+                   }
+           deriving (Eq, Generic, ToJSON, FromJSON)
+
+-- | Build a query for a document plus it's language
+buildQuery :: Text -> Text -> SearchSettings -> Query
+buildQuery q lang settings = Query q lang Nothing settings
