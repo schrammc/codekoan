48fc9e20e353aeb8ef7485a82dc54c73e2de9383
christof schramm <christof.schramm@gmx.de>
2016-09-07 22:08:30 +0200

    FEATURE: Extended foundation type for search service

--- ma-search-service/src/Thesis/SearchService/ApplicationType.hs
+++ ma-search-service/src/Thesis/SearchService/ApplicationType.hs
@@ -1,25 +1,59 @@
+{-# LANGUAGE GADTs #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TemplateHaskell #-}
 module Thesis.SearchService.ApplicationType where
 
 import           Control.Monad.IO.Class
 import           Control.Monad.Logger
+import           Control.Monad.Catch
 
+import           Data.Monoid ((<>))
 import qualified Data.Text as Text
 
 import           Network.AMQP
 import           Thesis.ServiceSettings
+import           Thesis.SearchService.InitializationException
 
-data Application =
-  Application{ appRabbitConnection :: !Connection
-             , appSettings         :: !ServiceSettings
-             , appQueue            :: !Text.Text
-             }
+import           Thesis.CodeAnalysis.Language
+import           Thesis.CodeAnalysis.Language.Java
+import           Thesis.CodeAnalysis.Language.Python
 
-buildFoundation :: (MonadIO m, MonadLogger m) => ServiceSettings -> m Application
+import           Thesis.Search.Index
+
+data Application where
+  Application :: { appRabbitConnection :: !Connection
+                 , appSettings         :: !ServiceSettings
+                 , appQueue            :: !Text.Text
+                 , appLanguage         :: Language t l
+                 , appIndex            :: SearchIndex t l
+                 }  -> Application
+
+buildFoundation :: (MonadThrow m, MonadIO m, MonadLogger m)
+                   => ServiceSettings
+                -> m Application
 buildFoundation settings@ServiceSettings{..} = do
+
   conn <- liftIO $ openConnection (Text.unpack $ rmqHost serviceRMQSettings)
                                   (rmqVirtualHost serviceRMQSettings)
                                   (rmqUser serviceRMQSettings)
                                   (rmqPassword serviceRMQSettings)
-  return $ Application conn settings "queries-java"
+
+  let buildApp :: forall t . forall l .
+                  Language t l
+               -> SearchIndex t l
+               -> Application
+      buildApp = Application conn settings "queries-java"
+
+  -- Building language specifics
+  case serviceLanguage of
+    "java" -> do
+      $(logInfo) "Application language: java"
+      return $ buildApp java undefined
+    "python" -> do
+      $(logInfo) "Application language: python"
+      return $ buildApp python undefined
+    _ -> do
+      $(logError) $ "Unrecognized language: " <> serviceLanguage
+      throwM InitializationException
