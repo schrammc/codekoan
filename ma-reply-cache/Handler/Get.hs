2b6396bef9de2448adaadb37eec13610f2d281c3
Christof Schramm <kryo@vm-10-155-208-4.cloud.mwn.de>
2016-09-20 15:44:16 +0200

    FEATURE: Only return complete replies from cache

--- ma-reply-cache/Handler/Get.hs
+++ ma-reply-cache/Handler/Get.hs
@@ -1,11 +1,14 @@
+{-# LANGUAGE TupleSections #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards #-}
 module Handler.Get where
 
 import Control.Concurrent.MVar
+import qualified Data.Text as Text
 import Foundation
 import Yesod.Core
 import Thesis.Messaging.Query
+import Thesis.Messaging.ResultSet
 import qualified Data.Map.Strict as M
 
 getGetR :: QueryId -> Handler Value
@@ -13,4 +16,17 @@ getGetR qId = do
   App{..} <- getYesod
 
   cacheContent <- liftIO $ readMVar appReplyCache
-  return $ object ["result" .= (M.lookup qId cacheContent)]
+
+  let resultValue :: Maybe (ResultSetMsg, Text.Text)
+      resultValue = do
+        val <- M.lookup qId cacheContent
+        let n = length val
+            clusterSize = resultClusterSize $ head val
+        if n == clusterSize
+          then mergeResultSetMsgs val >>= return . (, "finished")
+          else mergeResultSetMsgs val >>= return . (, "pending")
+  case resultValue of
+    Nothing -> return $ object [ "result" .= Null
+                               , "status" .= ("nothing" :: Text.Text)]
+    Just (v, msg) ->
+      return $ object ["result" .= v, "status" .= msg]
