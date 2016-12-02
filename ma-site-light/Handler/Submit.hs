a28986d57874ab5ad426983c2bebccf648de38dd
analytics <analytics@bangka.pms.ifi.lmu.de>
2016-12-02 12:51:54 +0100

    REFACTOR: Some cleanup in frontend

--- ma-site-light/Handler/Submit.hs
+++ ma-site-light/Handler/Submit.hs
@@ -108,25 +108,25 @@ waitForReply settings@AppSettings{..} queryId@(QueryId qId) = do
   
   let statusParser = withObject "reply object" $ \o -> o .: "status"
       statusMaybe = parseMaybe statusParser $ getResponseBody resp
+      resultParser = withObject "reply object" $ \o -> o .: "result"
+      resultMaybe :: forall a . FromJSON a => Maybe a
+      resultMaybe = parseMaybe resultParser $ getResponseBody resp
   case statusMaybe of
     Nothing -> fail "Invalid JSON (waitForReply)"
-    Just (t :: Text) | t == "finished" -> do
-      let resultParser = withObject "reply object" $ \o -> o .: "result"
-          resultMaybe  = parseMaybe resultParser $ getResponseBody resp
-      case resultMaybe of
-        Just r -> do
-          $(logInfo) $ "Received a complete result for query" <> (pack $ show qId)
-          return (Right r)
-        Nothing ->  fail $ "Failed to read JSON response to queryId " <> show qId
-                     | t == "exception" -> do
-      let p = withObject "reply object" $ \o -> o .: "result"
-          resultMaybe  = parseMaybe p $ getResponseBody resp
-      case resultMaybe of
-        Just reason -> return (Left reason)
-        Nothing -> return (Left "Unknown exception")
-                     | otherwise -> do
-                       liftIO $ threadDelay $ 1 * 1000 * 1000
-                       waitForReply settings queryId
+    Just (t :: Text)
+      | t == "finished" ->
+        case resultMaybe of
+          Just r -> do
+            $(logInfo) $ "Received a complete result for query" <> (pack $ show qId)
+            return (Right r)
+          Nothing ->  fail $ "Failed to read JSON response to queryId " <> show qId
+      | t == "exception" ->
+        case resultMaybe of
+          Just reason -> return (Left reason)
+          Nothing -> return (Left "Unknown exception")
+      | otherwise -> do
+          liftIO $ threadDelay $ 1 * 1000 * 1000
+          waitForReply settings queryId
     
   where
     req = parseRequest_ (appReplyCacheURL ++ show qId)
