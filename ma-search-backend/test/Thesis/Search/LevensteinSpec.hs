1fd2aa8e973654050ca71803003376b3296b40de
christof schramm <christof.schramm@gmx.de>
2017-01-25 16:30:31 +0100

    TEST: Document test case better

--- ma-search-backend/test/Thesis/Search/LevensteinSpec.hs
+++ ma-search-backend/test/Thesis/Search/LevensteinSpec.hs
@@ -13,23 +13,45 @@ spec = do
   describe "Thesis.Search.LevensteinSpec" $ do
     alwaysFindSuff
 
--- | Given a suffix tree is constructed for a set of nonempty strings. Then each
--- of these individual strings should be retrievable from the suffix tree by the
--- lookup function.
-alwaysFindSuff :: Spec
+-- | Given a suffix tree is constructed for a set of nonempty
+-- strings. Then each of these individual strings should be
+-- retrievable from the suffix tree by the lookup function.
+alwaysFindSuff :: SpecWith ()
 alwaysFindSuff = do
   it "Suffixtree lookup should always find each entry" $
+    -- This quickcheck property must hold for all lists of
+    -- strings. Quickcheck will check this property multiple times for
+    -- randomly generated lists of strings.
     property $ \(strs :: [String]) ->
+      -- This logic builds a suffix tree for each of the strings and
+      -- then merges these suffix trees together into a single
+      -- generalized suffix tree (GST)
       let vectors = V.fromList <$> (filter (\s -> length s > 1) strs)
+          -- Turn the given list of strings into a list of vectors
+          -- (i.e. arrays) of characters. Then number these vectors
+          -- from zero.
           indexedVectors = zip vectors (S.singleton <$> ([0..] :: [Int]))
-          tries = (\(tr,s) -> buildSuffixTrie Nothing tr s) <$> indexedVectors
+          tries = fmap (\(tr,s) -> buildSuffixTrie Nothing tr s)
+                       indexedVectors
           mergedTrie = foldl1 mergeTries tries
+
+      -- Go over all vectors of characters and their according number,
+      -- and lookup each in the merged suffix tree.
       in and $ do
         (v, i :: S.Set Int) <- indexedVectors
-        let results = lookupAllSuff (vectorToLevensteinAutomaton 0 v) mergedTrie 0
+        let results = lookupAllSuff
+                        -- A simple levenshtein automaton that only
+                        -- exactly accepts it's original word.
+                        (vectorToLevensteinAutomaton 0 v)
+                        -- Run on the whole merged trie.
+                        mergedTrie
+                        -- Minimal match length is set to zero.
+                        0
             resultSets = do
-              (_, setsAndPoss, _) <- results
-              (set, _) <- setsAndPoss
+              (_, setsAndPositions, _) <- results
+              (set, _) <- setsAndPositions
               return set
+        -- Make sure that the original vector is in the results
         return $ i `elem` resultSets
+
 
