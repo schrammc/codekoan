0bdbc1e56ca8f05021d275af11df9376529833ca
analytics <analytics@bangka.pms.ifi.lmu.de>
2017-03-07 17:13:39 +0100

    FEATURE: Make python parser logical lines more standard conforming

--- ma-language-python/src/Thesis/CodeAnalysis/Language/Python.hs
+++ ma-language-python/src/Thesis/CodeAnalysis/Language/Python.hs
@@ -30,7 +30,7 @@ import           Control.Monad.Trans.State
 import           Data.Attoparsec.Text as AP
 import           Data.Char
 import           Data.Conduit (Source)
-import           Data.Maybe (fromJust)
+import           Data.Maybe (fromJust, catMaybes)
 import qualified Data.Text as Text
 
 import           Thesis.Search.Index
@@ -65,8 +65,34 @@ buildIndexForPython = buildIndexFromAnswers python
 
 
 -- | State that the will use internally.
-newtype ParserState = ParserState{ indentationLevel :: [Int]
-                                 }
+data ParserState = ParserState { indentationLevel :: ![Int]
+                               , parenLevel   :: !Int
+                               , bracketLevel :: !Int
+                               , braceLevel   :: !Int
+                               }
+
+hasOpenParens :: ParserState -> Bool
+hasOpenParens state = parenLevel state /= 0 &&
+                      bracketLevel state /= 0 &&
+                      braceLevel state /= 0
+
+adjustParens :: [PyToken] -> ParserState -> ParserState
+adjustParens tks state =
+  let parenLevel' = parenLevel state + levelChange PyTokenLParen PyTokenRParen tks
+      brackLevel' = bracketLevel state + levelChange PyTokenLBrack
+                                                     PyTokenRBrack
+                                                     tks
+      braceLevel' = braceLevel state + levelChange PyTokenLBrace PyTokenRBrace tks
+  in state{ parenLevel = parenLevel'
+          , bracketLevel = brackLevel'
+          , braceLevel = braceLevel'
+          }
+
+levelChange :: Eq a => a -> a -> [a] -> Int
+levelChange _    _     [] = 0
+levelChange open close (x:xs) | x == open = 1 + (levelChange open close xs)
+                              | x == close = (levelChange open close xs) -1
+                              | otherwise = levelChange open close xs
 
 -- | Tokenize a piece of python code. It is critical, that this code is
 -- normalized beforehand. Normalization eliminates all tab characters in the
@@ -85,7 +111,7 @@ tokenizePy LanguageText{..} = buildTokenVector <$> parsedResult
       Left _  -> Nothing
     
     parseCode :: Parser [(Int, Maybe PyToken)]
-    parseCode = evalStateT parsePy (ParserState [0])
+    parseCode = evalStateT parsePy (ParserState [0] 0 0 0)
 
     -- | Recursive parser that parses an entire file while strining along some
     -- parser state. That state contains information on the depth of the current
@@ -94,19 +120,27 @@ tokenizePy LanguageText{..} = buildTokenVector <$> parsedResult
     parsePy :: StateT ParserState Parser [(Int, Maybe PyToken)]
     parsePy = do
       -- Generate indentation tokens if necessary
+      indentBefore <- get
       spaces <- getSpaces
 
       -- This parser will parse the conent of a line. A line in this context is
       -- not just a text-line. A text-line can also be terminated with a
       -- backslash. In such a case the follwing text-line is interpreted as a
       -- continuation of the current line. The backslash is normalized away.
-      let contentP = do
-            ts <- AP.many1 lenParser
+      let loglineP = do
+            ts <- lift $ AP.many1 lenParser
+
+            -- Adjust the parser state with the current braces, brackets and
+            -- curly braces
+            modify (adjustParens (catMaybes $ snd <$> ts))
+            currentState <- get
+
             case ts of
               [] -> return []
-              _ | snd (last ts) == Just PyTokenBackslash -> do
-                    e <- eols
-                    ts' <- contentP
+              _ | snd (last ts) == Just PyTokenBackslash
+                  || hasOpenParens currentState -> do
+                    e <- lift eols
+                    ts' <- loglineP
                     -- Using init ts here removes the backslash. In order to
                     -- still have correct ranges for following tokens, we
                     -- include a (1,Nothing) space.
@@ -114,7 +148,7 @@ tokenizePy LanguageText{..} = buildTokenVector <$> parsedResult
                 | otherwise -> return ts
 
       -- Parse the line's contents
-      tks <- lift contentP -- lift $ AP.many1 lenParser
+      tks <- loglineP
 
       --  Parse a number of linebreaks after the content of the line
       lbs <- (lift $ ((:[]) <$> eols)) <|> (return [])
@@ -122,10 +156,14 @@ tokenizePy LanguageText{..} = buildTokenVector <$> parsedResult
       
       rest <- parsePy <|> (lift $ endOfInput *> pure [])
 
-      let tokens = case tks of
-            [] -> (filter ((> 0) . fst) spaces) ++ lbs
-            _  -> spaces ++ tks ++ lbs
-      return $ tokens ++ rest
+      case tks of
+        [] -> do
+          -- lines with nothing but space and comment don't influence the
+          -- indentation level
+          put indentBefore
+
+          return $ (filter ((> 0) . fst) spaces) ++ lbs ++ rest
+        _  -> return $ spaces ++ tks ++ lbs ++ rest
 
     -- | Parses tokens, comments and horizontal whitespace along with
     -- information on the length of the consumed string.
