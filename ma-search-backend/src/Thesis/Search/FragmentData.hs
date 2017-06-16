c89db59c07e69bf93ba7ed0726236be7f709cc67
christof schramm <christof.schramm@gmx.de>
2017-06-16 18:44:56 +0200

    FEATURE: Add method to get fragment id from fragment data

--- ma-search-backend/src/Thesis/Search/FragmentData.hs
+++ ma-search-backend/src/Thesis/Search/FragmentData.hs
@@ -7,23 +7,31 @@
 --
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# Language FlexibleContexts #-}
 module Thesis.Search.FragmentData where
 
 import Thesis.Data.Stackoverflow.Answer
 import Control.DeepSeq
 import Data.Hashable
 
-
-class (Hashable a, NFData a, Ord a) => FragmentData a where
-  printFragData :: a -> String
+class ( Eq (FragmentId a), Hashable (FragmentId a)
+      , Hashable a, NFData a, Ord a) => FragmentData a where
+  type FragmentId a
   fragDataTokenLength :: a -> Int
+  printFragData :: a -> String
+  getFragmentId :: a -> FragmentId a
 
 instance FragmentData (Int, Int) where
+  type FragmentId (Int, Int) = Int
   printFragData (ann,_) = show ann
   fragDataTokenLength (_, l) = l
+  getFragmentId (i, _) = i
+
 
 instance FragmentData AnswerFragmentMetaData where
+  type FragmentId AnswerFragmentMetaData = AnswerFragmentId
   printFragData (AnswerFragmentMetaData AnswerFragmentId{..} _) =
     (show $ answerIdInt fragmentAnswerId) ++ " (" ++ (show $ fragmentId) ++ ")"
   fragDataTokenLength AnswerFragmentMetaData{..} = fragmentMetaSize
+  getFragmentId = fragmentMetaId
